615529c1668cbd3ce6264ae0f3a48ddad942c2b8
Jerry <x.jerry.wang@gmail.com>
2024-03-27 00:34:49 +0800

    feat: update halo proxy setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/ui',
-    '@nuxtjs/color-mode',
-    '@vueuse/nuxt',
-    '@vite-pwa/nuxt',
-    '@formkit/auto-animate/nuxt'
+    "@nuxt/ui",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@vite-pwa/nuxt",
+    "@formkit/auto-animate/nuxt",
   ],
   devtools: { enabled: true },
   routeRules: {
-    // todo
-     '/api/halo': { proxy: 'https://example.com' },
-  }
-})
+    // proxy halo request
+    // api
+    "/api/**": { proxy: "http://localhost:8090/api/**" },
+    "/apis/**": { proxy: "http://localhost:8090/apis/**" },
+    "/login/**": { proxy: "http://localhost:8090/login/**" },
+    "/actuator/**": { proxy: "http://localhost:8090/actuator/**" },
+    // static pages
+    "/uc/**": { proxy: "http://localhost:8090/uc/**" },
+    "/console/**": { proxy: "http://localhost:8090/console/**" },
+  },
+});
